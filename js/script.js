ce4d2503e25fd90474b581f5ccf2ef4638476404
Natalia Tereshchenko <xramono4ka@gmail.com>
2019-06-28 01:05:11 +0300

    Создана папка с проектом. Добавлен js файл.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let money = 100;
+//console.log(money);
+let income = 200;
+//console.log(income);
+let addExpenses = 50;
+//console.log(addExpenses);
+let deposit = 150;
+//console.log(deposit);
+let mission = 500;
+//console.log(mission);
+let period = 'Week';
+//console.log(period);
+
+alert(period);
+
+console.log('Mission Complete');
