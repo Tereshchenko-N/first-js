9ff96bedfd6c2c8df199899a0debc08d91a6b2b8
Natalia Tereshchenko <xramono4ka@gmail.com>
2019-06-28 21:30:19 +0300

    Исправлены ошибки

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@ let money = 4000,
   period = 5;
 let budgetDay = money / 30;
 
-console.log(mass);
+
 console.log(typeof (money));
 console.log(typeof (income));
 console.log(typeof (deposit));
 
 console.log(income.length);
 
-
-console.log(addExpenses.toLowerCase());
-
-console.log('Goal to earn:', mission);
 console.log('Month period: ', period);
+console.log('Goal to earn:', mission);
+
+console.log(addExpenses.toLowerCase().split(','));
+
 console.log('day budget:', budgetDay);
 console.log('remainder:', money % 30);
 
