cf09747dc722eab4f91265502357d18ce1de4f30
Natalia Tereshchenko <xramono4ka@gmail.com>
2019-07-02 21:51:33 +0300

    Усложненное задание 3го урока

--- project-hard03/js/myscript.js
+++ project-hard03/js/myscript.js
@@ -1,1 +1,24 @@
-let lang;
+let lang = prompt('Выберите язык: введите ru или en'),
+  rus = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'],
+  eng = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+
+if (lang === 'ru') {
+  console.log(rus);
+} else if (lang === 'en') {
+  console.log(eng);
+} else {
+  console.log("Некорректно выбран язык. Нужно ввести en или ru");
+  alert("Некорректно выбран язык. Нужно ввести en или ru");
+}
+
+
+let namePerson = prompt('Укажите Ваше имя');
+
+
+if (namePerson === 'Артем') {
+  console.log('директор');
+} else if (namePerson === 'Максим') {
+  console.log('преподаватель');
+} else {
+  console.log('студент');
+}
